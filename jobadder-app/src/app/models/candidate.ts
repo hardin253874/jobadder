4b6ab62f099903615bb73291d32f65ee49505931
hardin253874 <hardin.253874@gmail.com>
2019-09-10 00:23:10 +1000

    Add files via upload

--- jobadder-app/src/app/models/candidate.ts
+++ jobadder-app/src/app/models/candidate.ts
@@ -2,11 +2,13 @@ export class candidate {
   candidateId: number;
   name: string;
   skillTags: string;
+  matchRatio: number;
   constructor(
     fields?: {
       candidateId?: number;
       name?: string;
       skillTags?: string;
+      matchRatio?: number;
     }) {
     if (fields) Object.assign(this, fields);
   }
